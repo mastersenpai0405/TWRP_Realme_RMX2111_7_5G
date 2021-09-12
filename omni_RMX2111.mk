e67c1ff3b062cb573b373801540940d0c294205e
donsenpai05 <83948563+mastersenpai05@users.noreply.github.com>
2021-09-13 02:30:18 +0800

    update

--- omni_RMX2111.mk
+++ omni_RMX2111.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2111
